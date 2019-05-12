d98e45a30ddf6c52b64feb09f39cd8e9110221ff
luanngominh <luannm@dwarvesv.com>
2019-05-12 21:54:21 +0700

    jwt

--- ext/util/jwt/jwt.go
+++ ext/util/jwt/jwt.go
@@ -24,28 +24,28 @@ func NewJWTMiddleware(jwtSecurity *goa.JWTSecurity) (goa.Middleware, error) {
 		}
 		return nil
 	})
-	
+
 	return jwt.New(config.Cfg.SignKey, validationHandler, app.NewJWTSecurity()), nil
 }
 
-//GenerateJWTToken ...
-func GenerateJWTToken(userID, email string) (string, error) {
+//GenerateJWT ...
+func GenerateJWT(userID, email string) *jwtgo.Token {
 	token := jwtgo.New(jwtgo.SigningMethodRS512)
 	oneMonth := time.Now().Add(time.Duration(24*30) * time.Hour).Unix()
 	uuid := uuid.NewV4()
 
 	token.Claims = jwtgo.MapClaims{
-		"iss":     "Issuer",                          // who creates the token and signs it
-		"aud":     "Audience",                        // to whom the token is intended to be sent
-		"exp":     oneMonth,                          // time when the token will expire (10 minutes from now)
-		"jti":     uuid.String(),                     // a unique identifier for the token
-		"iat":     time.Now().Unix(),                 // when the token was issued/created (now)
-		"nbf":     2,                                 // time before which the token is not yet valid (2 minutes ago)
-		"sub":     "llra api token",                  // the subject/principal is whom the token is about
-		"user.id": userID,              // user id - not a standard claim
-		"user.email": email, // user email - not a standard claim
-		"scopes":  "api:access company_account:true", 
+		"iss":        "Issuer",          // who creates the token and signs it
+		"aud":        "Audience",        // to whom the token is intended to be sent
+		"exp":        oneMonth,          // time when the token will expire (10 minutes from now)
+		"jti":        uuid.String(),     // a unique identifier for the token
+		"iat":        time.Now().Unix(), // when the token was issued/created (now)
+		"nbf":        2,                 // time before which the token is not yet valid (2 minutes ago)
+		"sub":        "llra api token",  // the subject/principal is whom the token is about
+		"user.id":    userID,            // user id - not a standard claim
+		"user.email": email,             // user email - not a standard claim
+		"scopes":     "api:access company_account:true",
 	}
 
-	return token.SignedString()
+	return token
 }
